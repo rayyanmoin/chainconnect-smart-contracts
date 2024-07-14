1fcd78d6b41785eeaa05c64bf9b49c02466de201
rayyanmoin <rayyansaqibmoin@gmail.com>
2024-07-14 11:49:52 +0500

    added validate name and lower case string to add more exceptions

--- contracts/AccountCreation.sol
+++ contracts/AccountCreation.sol
@@ -1,9 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.26;
 
-contract AccountCreation{
-
-    struct UserData{
+contract AccountCreation {
+    struct UserData {
         string username;
         string displayName;
         string imageHash;
@@ -13,23 +12,53 @@ contract AccountCreation{
     mapping(address => UserData) user;
     mapping(string => bool) existsUser;
 
-    function createAccount(UserData memory userInfo) external{
-        require(existsUser[userInfo.username],"username exists");
-        existsUser[userInfo.username] = true;
+    function createAccount(UserData memory userInfo) external {
+        require(!validateName(userInfo.username), "Invalid");
+        require(existsUser[toLower(userInfo.username)], "username exists");
+        existsUser[toLower(userInfo.username)] = true;
         user[msg.sender] = userInfo;
     }
 
-    function updateAccount(UserData memory userInfo)external{
-        require(!existsUser[userInfo.username],"username exists");
-        existsUser[userInfo.username] = true;
-        existsUser[user[msg.sender].username] = false;
+    function updateAccount(UserData memory userInfo) external {
+        require(!validateName(userInfo.username), "Invalid");
+        require(!existsUser[toLower(userInfo.username)], "username exists");
+        existsUser[toLower(userInfo.username)] = true;
+        existsUser[toLower(user[msg.sender].username)] = false;
 
         user[msg.sender] = userInfo;
+    }
+    function validateName(string memory str) public pure returns (bool) {
+        bytes memory b = bytes(str);
+        if (b.length < 1) return false;
+        if (b.length > 25) return false; // Cannot be longer than 25 characters
 
-            
+        for (uint256 i; i < b.length; i++) {
+            bytes1 char = b[i];
 
+            if (
+                !(char >= 0x30 && char <= 0x39) && //9-0
+                !(char >= 0x41 && char <= 0x5A) && //A-Z
+                !(char >= 0x61 && char <= 0x7A) //a-z
+            ) return false;
+        }
+
+        return true;
     }
 
-    
-
+    /**
+     * @dev Converts the string to lowercase
+     */
+    function toLower(string memory str) public pure returns (string memory) {
+        bytes memory bStr = bytes(str);
+        bytes memory bLower = new bytes(bStr.length);
+        for (uint256 i = 0; i < bStr.length; i++) {
+            // Uppercase character
+            if ((uint8(bStr[i]) >= 65) && (uint8(bStr[i]) <= 90)) {
+                bLower[i] = bytes1(uint8(bStr[i]) + 32);
+            } else {
+                bLower[i] = bStr[i];
+            }
+        }
+        return string(bLower);
+    }
 }
